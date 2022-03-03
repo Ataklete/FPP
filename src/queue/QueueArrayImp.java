7be3844a7ab6d7aeb57f77061778605ccb954824
Ataklete <65136939+Ataklete@users.noreply.github.com>
2022-03-03 01:16:07 -0500

    Update QueueArrayImp.java

--- src/queue/QueueArrayImp.java
+++ src/queue/QueueArrayImp.java
@@ -1,54 +1,41 @@
 package queue;
-
 import java.util.Arrays;
-
-public class QueueArrayImp {
-	
+public class QueueArrayImp {	
 	private int[] arr = new int[10];
 	private int size = 0;
 	private int front = 0;
-	private int rear = 0;
-	
+	private int rear = 0;	
    public boolean isEmpty() {
 	 return  size == 0;
-   }
-   
+   }   
    public void resize() {
 	   int[] temp = new int[arr.length * 2];
 	   System.arraycopy(arr, front, temp, front, arr.length);
 	   arr = temp;
-   }  
-   
+   }     
    public int peek() {
 	   if(isEmpty())
 		   return 0;
 	   else 
 	   return arr[front];
    }
-   
    public void enqueue(int x) {
 	   if (rear == arr.length)  resize();
 	   arr[rear] = x;
 	   rear++;
 	   size++;
    }
-   
    public int dequeue() {
 	   if (front == -1) throw new IllegalStateException("cannot be null");
 	   int oldfront = arr[front] ;
 	   front++;
 	   size--;
-	   return oldfront;
-	   
+	   return oldfront;   
    }
-   
-   
-	
 	@Override
 public String toString() {
 	return   Arrays.toString(arr);//+ ", size=" + size + ", front=" + front + ", rear=" + rear;
 }
-
 	public static void main(String[] args)   {
 		
 		QueueArrayImp q = new QueueArrayImp();
@@ -61,5 +48,4 @@ public String toString() {
 		System.out.println(q.peek());
         System.out.println(q);
 	}
-
 }
